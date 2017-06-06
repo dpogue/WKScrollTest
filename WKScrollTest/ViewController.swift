39798c6e15c744f22942a07bb3d890bd5fccf54c
Darryl Pogue <darryl@ayogo.com>
2017-06-06 11:42:14 -0700

    Update test to demo viewport-fit WK/UI

--- WKScrollTest/ViewController.swift
+++ WKScrollTest/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 import WebKit
 
+let USE_UIWEBVIEW = false; // false, or true
+let VIEWPORT_FIT = "cover"; // "cover", "contain", or "auto"
+let POSITION = "fixed"; // "fixed", or "sticky"
+
 class ViewController: UIViewController {
     
     @IBOutlet var containerView : UIView! = nil
     var webView: WKWebView?
+    var oldView: UIWebView?
 
     override func loadView() {
         super.loadView()
         
-        self.webView = WKWebView()
-        self.webView!.scrollView.bounces = false;
-        self.view = self.webView!
+        if (USE_UIWEBVIEW) {
+            self.oldView = UIWebView()
+            self.oldView!.scrollView.bounces = false;
+            self.view = self.oldView!
+        } else {
+            self.webView = WKWebView()
+            self.webView!.scrollView.bounces = false;
+            self.view = self.webView!
+        }
     }
 
     override func viewDidLoad() {
@@ -28,15 +39,15 @@ class ViewController: UIViewController {
         let doc = ["<!DOCTYPE html>",
                    "<html>",
                    " <head>",
-                   "  <meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0,user-scalable=no,maximum-scale=1.0,minimum-scale=1.0\">",
+                   "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=\(VIEWPORT_FIT)\">",
                    "  <style>",
-                   "   body { font-family: -apple-system; font-size: 1.25em; padding-top: 80px; padding-bottom: 50px; }",
-                   "   header { position: fixed; height: 50px; padding-top: 30px; width: 100%; top: 0; left: 0; background: lightsteelblue; text-align: center; }",
-                   "   footer { position: fixed; height: 30px; width: 100%; bottom: 0; left: 0; background: steelblue; text-align: center; }",
+                   "   html { font: -apple-system-body; }",
+                   "   body { font-family: -apple-system; font-size: 1.25em; padding-top: 2.5em; padding-bottom: 1.5em; margin: 0; padding: 0; }",
+                   "   header { position: \(POSITION); height: 2.5em; padding-top: 30px; width: 100%; top: 0; left: 0; background: lightsteelblue; text-align: center; }",
+                   "   footer { position: \(POSITION); height: 1.5em; width: 100%; bottom: 0; left: 0; background: steelblue; text-align: center; }",
+                   "   p { padding: auto 1em; }",
+                   "   input { font-size: 0.75em; }",
                    "  </style>",
-                   "  <script>",
-                   "   function logClick() { navigator.geolocation.getCurrentPosition(); }",
-                   "  </script>",
                    " </head>",
                    " <body>",
                    "  <header>",
@@ -52,34 +63,38 @@ class ViewController: UIViewController {
                    "  </p>",
                    "  <ol>",
                    "   <li><h3>💚 Test Case 1:</h3>",
-                   "    <p>Scroll down the page. Tap the input element in the header. With the keyboard overlay shown, scroll up the page. The fixed position header should remain attached to the top of the screen.</p>",
+                   "    <p>Scroll down the page. Tap the input element in the header. With the keyboard overlay shown, scroll up the page. The \(POSITION) position header should remain in position and attached to the top of the screen.</p>",
                    "   </li>",
                    "   <li><h3>💔 Test Case 2:</h3>",
-                   "    <p>Tap the input element in the header. With the keyboard overlay shown, scroll down the page. The fixed position header should remain attached to the top of the screen.</p>",
+                   "    <p>Tap the input element in the header. With the keyboard overlay shown, scroll down the page. The \(POSITION) position header should remain in position and attached to the top of the screen.</p>",
                    "   </li>",
                    "   <li><h3>💔 Test Case 3:</h3>",
-                   "    <p>Tap the input element here:<br><input type=\"text\" /><br> When the keyboard is shown, the fixed position header should remain attached to the top of the screen.</p>",
+                   "    <p>Tap the input element here:<br><input type=\"text\" /><br> When the keyboard is shown, the \(POSITION) position header should remain in position and attached to the top of the screen.</p>",
                    "   </li>",
                    "   <li><h3>💔 Test Case 4:</h3>",
-                   "    <p>Tap the input element in the footer. When the keyboard is shown, the fixed position header should remain attached to the top of the screen.</p>",
+                   "    <p>Tap the input element in the footer. When the keyboard is shown, the \(POSITION) position header should remain in position and attached to the top of the screen.</p>",
                    "   </li>",
                    "   <li><h3>💚 Test Case 5:</h3>",
-                   "    <p>Tap the input element in the footer. With the keyboard overlay shown, scroll down the page. The fixed position footer should remain attached to the bottom of the screen.</p>",
+                   "    <p>Tap the input element in the footer. With the keyboard overlay shown, scroll down the page. The \(POSITION) position footer should remain in position and attached to the bottom of the screen.</p>",
                    "   </li>",
                    "   <li><h3>💔 Test Case 6:</h3>",
-                   "    <p>Tap the input element in the footer. With the keyboard overlay shown, scroll up the page. The fixed position footer should remain attached to the bottom of the screen.</p>",
+                   "    <p>Tap the input element in the footer. With the keyboard overlay shown, scroll up the page. The \(POSITION) position footer should remain in position and attached to the bottom of the screen.</p>",
                    "   </li>",
                    "   <li><h3>💔 Test Case 7:</h3>",
-                   "    <p>Tap the input element here:<br><input type=\"text\" /><br> When the keyboard is shown, the fixed position footer should remain attached to the bottom of the screen.</p>",
+                   "    <p>Tap the input element here:<br><input type=\"text\" /><br> When the keyboard is shown, the \(POSITION) position footer should remain in position and attached to the bottom of the screen.</p>",
                    "   </li>",
                    "   <li><h3>💔 Test Case 8:</h3>",
-                   "    <p>Tap the input element in the header. When the keyboard is shown, the fixed position footer should remain attached to the bottom of the screen.</p>",
+                   "    <p>Tap the input element in the header. When the keyboard is shown, the \(POSITION) position footer should remain in position and attached to the bottom of the screen.</p>",
                    "   </li>",
                    "  </ol>",
                    " </body>",
                    "</html>"];
 
-        self.webView!.loadHTMLString(doc.joined(separator:"\n"), baseURL: nil);
+        if (USE_UIWEBVIEW) {
+            self.oldView!.loadHTMLString(doc.joined(separator:"\n"), baseURL: nil);
+        } else {
+            self.webView!.loadHTMLString(doc.joined(separator:"\n"), baseURL: nil);
+        }
     }
 
     override func didReceiveMemoryWarning() {
